5b35a15090de934207cd0fe1abe727a874877073
Anjila Tripathi <anjila.tripathi@gmail.com>
2024-02-04 11:49:07 +0545

    static member and Method Example

---
+++ StaticMemberAndMethod/src/StaticEx.java
@@ -0,0 +1,34 @@
+
+class StudentInfo {
+
+    int id;
+    String name;
+    static String collegename = "XYZ";
+
+    public StudentInfo(int i, String n) {
+        id = i;
+        name = n;
+    }
+
+    public void display() {
+        System.out.println("Student Id: \t" + id);
+        System.out.println("Student name" + name);
+        System.out.println("Student College name" + collegename);
+    }
+
+    static public void changeCName() {
+        collegename = "Padma Kanya Multiple Campus";
+    }
+}
+
+public class StaticEx {
+
+    public static void main(String[] args) {
+        StudentInfo s= new StudentInfo(101,"Anjila");
+      s.display();
+      s.changeCName();
+      s.display();
+      
+
+    }
+}
