24050c678386cfe5ff3775338958c1c21f0f897f
Anjila Tripathi <anjila.tripathi@gmail.com>
2024-02-04 12:34:46 +0545

    Method Overriding

---
+++ MethodOverriding/src/methodoverriding/MethodOverriding.java
@@ -0,0 +1,32 @@
+class A
+{
+    public void show()
+    {
+        System.out.println("This is Class A");
+    }
+}
+class B extends A
+{
+    public void show()
+    {
+          super.show();
+           System.out.println("This is Class B");
+    }
+}
+//class C extends B
+//{
+//    public void show()
+//    {
+//           System.out.println("This is Class C");
+//    }
+//}
+
+public class MethodOverriding
+{
+    public static void main( String[] args)
+    {
+       B b = new B();
+       b.show();
+       
+    }
+}
