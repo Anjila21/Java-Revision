9fc21450138928355e51ff81acf7aea9827fc8ba
Anjila Tripathi <anjila.tripathi@gmail.com>
2024-02-05 17:41:57 +0545

    Final Keyword Example

---
+++ FinalKeywordEx/src/finalkeywordex/FinalKeywordEx.java
@@ -0,0 +1,11 @@
+public class FinalKeywordEx
+{
+    public static void main(String[] args)
+    {
+        int a = 10;
+       final  int b = 20;
+        a = 20;
+        b = 30;
+        System.out.println(a+"\n"+b);
+    }
+}
